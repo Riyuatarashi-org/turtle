d5bc6b84f8dac0f9a346f3fb1ddd244a4f1b856f
moeetomoni <dev@moee.fr>
2023-03-11 16:30:40 +0100

    3-feat(ctrl): create property user containing null|User

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,9 +1,10 @@
 <?php
 
-declare(strict_types = 1);
+declare( strict_types=1 );
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -14,4 +15,17 @@ class Controller extends BaseController
     use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
+
+    protected readonly ?User $user;
+
+    public function __construct()
+    {
+        /** @var \Illuminate\Contracts\Auth\Guard $auth */
+        $auth = auth();
+
+        /** @var null|\App\Models\User $user */
+        $user = $auth->user();
+
+        $this->user = $user;
+    }
 }
