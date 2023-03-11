4294c6ab64976fdb9b3d38bd71f1eb8b681fed7b
moeetomoni <dev@moee.fr>
2023-03-11 16:28:55 +0100

    3-feat(request): create Message requests

---
+++ app/Http/Requests/UpdateMessageRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateMessageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $user = $this->user();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        return $user->can('update', Message::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array<string>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'message'      => [ 'required', 'string', 'max:65535' ],
+
+            // Relations
+            'author_id'    => [ 'prohibited' ],
+            'recipient_id' => [ 'prohibited' ],
+
+            // Timestamps
+            'sent_at'      => [ 'prohibited' ],
+            'received_at'  => [ 'nullable', 'date_format:Y-m-d H:i:s' ],
+            'read_at'      => [ 'nullable', 'date_format:Y-m-d H:i:s' ],
+        ];
+    }
+}
