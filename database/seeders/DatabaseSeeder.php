78278c3ade31d7945bc01d04120466943a6d7d6b
moeetomoni <dev@moee.fr>
2023-03-11 16:32:21 +0100

    3-feat(seeder): seed a default admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,24 +1,32 @@
 <?php
 
-declare(strict_types = 1);
+declare( strict_types=1 );
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if (App::environment('local')) {
+            UserFactory::new()
+                       ->create([
+                           'name'              => 'Admin',
+                           'email'             => 'dev@moee.fr',
+                           'email_verified_at' => now(),
+                           'password'          => Hash::make('admin'),
+                       ]);
+        }
     }
 }
